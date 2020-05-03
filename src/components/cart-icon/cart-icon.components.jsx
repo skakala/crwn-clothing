a8778f06bde69e27e0ad306661ffd58814506f94
skala <skakala@wp.pl>
2020-05-03 16:10:13 +0200

    added CartIcon and CartDropDown Components to header componnent

---
+++ src/components/cart-icon/cart-icon.components.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./cart-icon.style.scss";
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import { connect } from "react-redux";
+import { toggleCartHiden } from "../../redux/cart/cart.actions";
+
+const CartIcon = ({ toggleCartHiden }) => (
+  <div className="cart-icon" onClick={toggleCartHiden}>
+    <ShoppingIcon className="shopping-icon" />
+    <span className="item-count">0</span>
+  </div>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHiden: () => dispatch(toggleCartHiden()),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
--- src/components/header/header-component.jsx
+++ src/components/header/header-component.jsx
@@ -4,8 +4,10 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/original.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.components";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo"></Logo>
@@ -27,12 +29,16 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+
+      <CartIcon />
     </div>
+    {hidden ? null : <CartDropDown></CartDropDown>}
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
